690606178703de71fee600e873a725f46bb0d4e3
llucumi <luz.lucumi@mercadolibre.com.co>
2022-06-28 11:23:04 -0500

    Ejercicio en clase

---
+++ Practiceinclass/ejercicioDos.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var(
+		temperatura int = 11
+		humedad float64 = 80.0
+		presion float64 = 1017
+		analisis bool45 = true
+	)
+
+	fmt.Println(analisis,"Temperatura",temperatura,"\nHumedad",humedad,"\nPresión",presion)
+}
---
+++ Practiceinclass/ejercicioUno.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nombre,direccion string = "Cristian","Cra 10 # 25-45"
+
+	fmt.Println("Nombre:",nombre,"\nDirección:",direccion)
+}
