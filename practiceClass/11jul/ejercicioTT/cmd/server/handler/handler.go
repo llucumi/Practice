74746f166aad07a745582297c5723c9cfddb198f
llucumi <luz.lucumi@mercadolibre.com.co>
2022-07-21 21:30:54 -0500

    Updating my gitaHub

--- practiceClass/11jul/ejercicioTT/cmd/server/handler/handler.go
+++ practiceClass/11jul/ejercicioTT/cmd/server/handler/handler.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"time"
 
-	users "ejercicioTM/internal/users"
-	"ejercicioTM/pkg/web"
+	users "ejercicioTT/internal/users"
+	"ejercicioTT/pkg/web"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,7 +20,7 @@ type request struct {
 	Edad     int       `json:"edad"`
 	Altura   float64   `json:"altura"`
 	Activo   bool      `json:"activo"`
-	Fecha    time.Time `json:"fecha"`
+	Fecha    time.Time `json:"fecha" example:"2022-07-12T00:53:16.535668Z" format:"date-time"`
 }
 
 type Usuarios struct {
@@ -31,6 +31,19 @@ func NewUser(s users.Service) *Usuarios {
 	return &Usuarios{service: s}
 }
 
+// Update Users godoc
+// @Summary Update a user by Id
+// @Tags Usuarios
+// @ID update-user-by-id
+// @Accept  json
+// @Produce json
+// @Param token header string true "token"
+// @Param id path string true "id from user to update"
+// @Param usuario body request true "User to update"
+// @Success 200 {object} web.Response
+// @Failure 400 {object} web.Response
+// @Failure 409 {object} web.Response
+// @Router /usuarios/{id} [put]
 func (u *Usuarios) Update() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -50,8 +63,28 @@ func (u *Usuarios) Update() gin.HandlerFunc {
 			return
 		}
 		//Nombre Apellido Email Edad Altura
-		if validate := validar(req); validate != "" {
-			c.JSON(400, web.NewResponse(400, nil, validate))
+		if req.Nombre == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el nombre del usuario es requerido"))
+			return
+		}
+
+		if req.Apellido == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el apellido del usuario es requerido"))
+			return
+		}
+
+		if req.Email == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el email del usuario es requerido"))
+			return
+		}
+
+		if req.Edad == 0 {
+			c.JSON(400, web.NewResponse(400, nil, "error: la edad del usuario es requerido"))
+			return
+		}
+
+		if req.Altura == 0.0 {
+			c.JSON(400, web.NewResponse(400, nil, "error: la altura del usuario es requerido"))
 			return
 		}
 
@@ -65,6 +98,19 @@ func (u *Usuarios) Update() gin.HandlerFunc {
 	}
 }
 
+// Patch Users godoc
+// @Summary Patch a user by Id
+// @Tags Usuarios
+// @ID update-user-by-id
+// @Accept  json
+// @Produce json
+// @Param token header string true "token"
+// @Param id path string true "id from user to patch"
+// @Param usuario body request true "User to patch"
+// @Success 200 {object} web.Response
+// @Failure 400 {object} web.Response
+// @Failure 409 {object} web.Response
+// @Router /usuarios/{id} [patch]
 func (u *Usuarios) UpdateLastAge() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -105,6 +151,16 @@ func (u *Usuarios) UpdateLastAge() gin.HandlerFunc {
 	}
 }
 
+// DeleteUsers godoc
+// @Summary Delete a user by Id
+// @Tags Usuarios
+// @ID delete-user-by-id
+// @Produce json
+// @Param token header string true "token"
+// @Param id path string true "id from user to delete"
+// @Success 200 {object} web.Response
+// @Failure 404 {object} web.Response
+// @Router /usuarios/{id} [delete]
 func (u *Usuarios) Delete() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -129,6 +185,15 @@ func (u *Usuarios) Delete() gin.HandlerFunc {
 	}
 }
 
+// ListUsers godoc
+// @Summary List users
+// @Tags Usuarios
+// @Description get usuarios
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Success 200 {object} web.Response
+// @Router /usuarios [get]
 func (u *Usuarios) GetAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -146,6 +211,18 @@ func (u *Usuarios) GetAll() gin.HandlerFunc {
 	}
 }
 
+// StoreUsers godoc
+// @Summary Store usuarios
+// @Tags Usuarios
+// @Description store usuarios
+// @Accept  json
+// @Produce  json
+// @Param token header string true "token"
+// @Param usuario body request true "User to store"
+// @Success 200 {object} web.Response
+// @Failure 400 {object} web.Response
+// @Failure 409 {object} web.Response
+// @Router /usuarios [post]
 func (u *Usuarios) Store() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("token")
@@ -160,8 +237,28 @@ func (u *Usuarios) Store() gin.HandlerFunc {
 			return
 		}
 		//Nombre Apellido Email Edad Altura
-		if validate := validar(req); validate != "" {
-			c.JSON(400, web.NewResponse(400, nil, validate))
+		if req.Nombre == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el nombre del usuario es requerido"))
+			return
+		}
+
+		if req.Apellido == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el apellido del usuario es requerido"))
+			return
+		}
+
+		if req.Email == "" {
+			c.JSON(400, web.NewResponse(400, nil, "error: el email del usuario es requerido"))
+			return
+		}
+
+		if req.Edad == 0 {
+			c.JSON(400, web.NewResponse(400, nil, "error: la edad del usuario es requerido"))
+			return
+		}
+
+		if req.Altura == 0.0 {
+			c.JSON(400, web.NewResponse(400, nil, "error: la altura del usuario es requerido"))
 			return
 		}
 
@@ -173,25 +270,4 @@ func (u *Usuarios) Store() gin.HandlerFunc {
 
 		c.JSON(200, web.NewResponse(200, u, ""))
 	}
-}
-
-// Función para validar campos de request
-func validar(req request) string {
-	var response string = "falta el/los campos: "
-	if req.Nombre == "" {
-		response += "Nombre, "
-	}
-	if req.Apellido == "" {
-		response += "Apellido, "
-	}
-	if req.Email == "" {
-		response += "Email, "
-	}
-	if req.Edad == 0 {
-		response += "Edad, "
-	}
-	if req.Altura == 0.0 {
-		response += "Altura"
-	}
-	return response
 }
---
+++ practiceClass/C1 - TT web/ejercicioConQuery.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type usuarios struct {
+	Id       int       `json:"id" binding:"required"`
+	Nombre   string    `json:"nombre"`
+	Apellido string    `json:"apellido"`
+	Email    string    `json:"email"`
+	Edad     int       `json:"edad"`
+	Altura   float64   `json:"altura"`
+	Activo   bool      `json:"activo"`
+	Fecha    time.Time `json:"fecha"`
+}
+
+//Este handler se encargará de responder a /.
+func PaginaInicio(ctx *gin.Context) {
+	ctx.String(200, "¡Hola, Bienvenido a mi primer ejercicio con Query!")
+}
+
+//Este handler se encargará de responder a /usuarios
+func AllUsers(ctx *gin.Context) {
+	user := usersJson()
+	ctx.JSON(200, gin.H{
+		fmt.Sprintf("Todos los usuarios"): user,
+	})
+}
+
+//Ejercicio 2
+//Este handler verificará si la id que pasa el usuario existe en nuestra
+// base de datos, de existir, retorna la info del usuario
+func BuscarUsuariosId(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	var exist bool
+	if err != nil {
+		ctx.String(400, "El id ingresado no existe")
+		return
+	}
+	user := usersJson()
+	for _, valueUser := range user {
+		if valueUser.Id == id {
+			exist = true
+			ctx.JSON(200, gin.H{
+				"Usuario encontrado": valueUser,
+			})
+			return
+		}
+	}
+	if exist == false {
+		ctx.String(400, "El id ingresado no existe")
+		return
+	}
+}
+
+func FiltrarUsuarios(c *gin.Context) {
+	listaUsuarios := usersJson()
+	var u usuarios
+	if c.ShouldBindQuery(&u) == nil {
+		// Setea las variables del c.Query("nombrevar")
+		log.Println(u.Id)
+		log.Println(u.Nombre)
+		log.Println(u.Apellido)
+		log.Println(u.Email)
+		log.Println(u.Edad)
+		log.Println(u.Altura)
+		log.Println(u.Activo)
+		log.Println(u.Fecha)
+	}
+
+	var filtradoUsuarios []*usuarios
+	for _, us := range listaUsuarios {
+		// filtrado por todos los campos
+		if u.Id == us.Id {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Nombre == us.Nombre {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Apellido == us.Apellido {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Email == us.Email {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Edad == us.Edad {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Altura == us.Altura {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Activo == us.Activo {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+		if u.Fecha == us.Fecha {
+			filtradoUsuarios = append(filtradoUsuarios, us)
+		}
+	}
+
+	c.JSON(http.StatusOK, filtradoUsuarios)
+	// devovemos el array filtrado
+}
+
+func usersJson() []*usuarios {
+	//Leo el json y lo envío como retorno
+	jsonUsers, err := os.ReadFile("./users.json")
+	if err != nil {
+		fmt.Print(err)
+	}
+	var users []*usuarios
+	err = json.Unmarshal(jsonUsers, &users)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return users
+}
+
+func main() {
+	server := gin.Default()
+	server.GET("/", PaginaInicio)
+	router := server.Group("/usuarios")
+	router.GET("/", AllUsers)
+	//Ejercicio 2
+	router.GET("/:id", BuscarUsuariosId)
+	//Ejercicio 1
+	router.GET("/filtrarusuario", FiltrarUsuarios)
+
+	server.Run()
+}
