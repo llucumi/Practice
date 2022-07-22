b17cc64688e81edd2b413c1f43439ffc10915ce9
llucumi <luz.lucumi@mercadolibre.com.co>
2022-07-22 15:17:55 -0500

    Errors

---
+++ practiceClass/22jul/calculadora.go
@@ -0,0 +1,13 @@
+package main
+
+// Función que recibe dos enteros y retorna la suma resultante
+func Sumar(num1, num2 int) int {
+	return num1 + num2
+	// Esta función ahora devuelve un resultado INCORRECTO
+	// return num1 - num2
+}
+
+// Función que recibe dos enteros y retorna la resta o diferencia resultante
+func Restar(num1, num2 int) int {
+	return num1 - num2
+}
